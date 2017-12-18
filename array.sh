8866b5134eedf2a7acfa5a1d7612031464ea0502
raghu <raghu@elantecs.com>
2017-12-18 22:16:58 +0000

    array.sh created

---
+++ array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This script is for basic  array list and loop for display
+
+SERVERLIST=("websrv01" "websrv02" "websrv03" "websrv04")
+COUNT=0
+
+for INDEX in ${SERVERLIST[@]}; do
+ echo "processing server: ${SERVERLIST[COUNT]}"
+  COUNT=" expr $COUNT + 1 "
+done
+
