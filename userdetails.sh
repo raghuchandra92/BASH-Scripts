6161ef039afe0e7d45c168ed566e5ab1f02ba35c
raghu <raghu@elantecs.com>
2018-02-04 04:02:57 +0000

    new file

---
+++ userdetails.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#This is a sample script to take user details 
+
+ ~/.bash_profile
+
+MYUSERNAME="username"
+MYPASSWORD="password123"
+STARTOFSCRIPT= 'date'
+
+echo "My login for the application is : $MYUSERNAME"
+echo "My login password for the application is : $MYPASSWORD"
+echo "I have started this script at : $STARTOFSCRIPT"
+echo " "
+echo " "
+echo " "
+echo " "
+
+
+ENDOFSCRIPT= 'date'
+
+echo "I ended the script at : $ENDOFSCRIPT"
